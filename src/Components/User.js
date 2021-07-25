da7dcd652ad83ff9316be1d4d0a03cc492c638b0
RayMaroun <raymondmaroun15@gmail.com>
2021-07-25 16:46:57 +0300

    Update User Component

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-function User(props) {
-  return (
-    <div>
-      <h2 style={{ backgroundColor: props.backColor }}>
-        Hello, {props.firstName} {props.lastName}!
-      </h2>
-      <img src={props.avatarUrl} width="370" height="300" />
-    </div>
-  );
+class User extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2 style={{ backgroundColor: this.props.backColor }}>
+          Hello, {this.props.firstName} {this.props.lastName}!
+        </h2>
+        <img src={this.props.avatarUrl} width="370" height="300" />
+      </div>
+    );
+  }
 }
 
 export default User;
